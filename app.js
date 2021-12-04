2d632faf703505d3c2d252cd0860dcbdc139186d
Fatih Yavuz <ibrahim.fatih.yavuz@gmail.com>
2021-12-04 20:24:40 +0100

    modified app.js

--- app.js
+++ app.js
@@ -3,24 +3,6 @@ const forecast = require('./utils/forecast')
 
 address = process.argv[2]
 
-// if(!address){
-//     console.log('Please provided address')
-// } else {
-//     geocode(address,(error,data)=>{
-//         if (error){
-//             return console.log(error)
-//         }
-    
-//         forecast(data.latitude,data.longtitude,(error,forecastData)=>{
-//             if(error){
-//                 return console.log(error)
-//             }
-//             console.log(data.placeName)    
-//             console.log('Data:',forecastData)
-//         })
-//     })
-// }
-
 if(!address){
     console.log('Please provided address')
 } else {
